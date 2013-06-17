6f49f9f4f1e0ecace2dfbe9c6939fa54e1601c9b
Ionuț G. Stan <ionut.g.stan@gmail.com>
2013-06-17 15:46:45 +0300

    Add package-level aliases for common ScalaTest types

--- src/main/g8/test/scala/$name__Camel$Test.scala
+++ src/main/g8/test/scala/$name__Camel$Test.scala
@@ -1,8 +1,5 @@
 package $organization$.$name$
 
-import org.scalatest.FunSuite
-import org.scalatest.matchers.MustMatchers
-
 class $name;format="Camel"$ extends FunSuite with MustMatchers {
   test("something") {
     true must be(true)
---
+++ src/main/g8/test/scala/package.scala
@@ -0,0 +1,6 @@
+package $organization$
+
+package object $name$ {
+  type FunSuite     = org.scalatest.FunSuite
+  type MustMatchers = org.scalatest.matchers.MustMatchers
+}

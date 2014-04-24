7fd89a5bca6951935687a19448f413221a8964a7
Ionuț G. Stan <ionut.g.stan@gmail.com>
2014-04-24 11:42:14 +0300

    Color only warn and error log entries
    Makes it easier to spot them in a sea of info or debug entries.

--- src/main/g8/src/test/scala/logback.scala
+++ src/main/g8/src/test/scala/logback.scala
@@ -9,11 +9,8 @@ import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase
 class LogLevelHighlighting extends ForegroundCompositeConverterBase[ILoggingEvent] {
   override def getForegroundColorCode(event: ILoggingEvent): String = {
     (event.getLevel.toInt: @annotation.switch) match {
-      case Level.INFO_INT  => CYAN_FG
-      case Level.WARN_INT  => YELLOW_FG
       case Level.ERROR_INT => RED_FG
-      case Level.DEBUG_INT => MAGENTA_FG
-      case Level.TRACE_INT => BLUE_FG
+      case Level.WARN_INT  => YELLOW_FG
       case _               => DEFAULT_FG
     }
   }

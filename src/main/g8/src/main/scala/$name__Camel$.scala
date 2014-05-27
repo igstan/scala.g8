8e895cf6e20aa6cf6a6f0c03f3254a25e7870847
Ionuț G. Stan <ionut.g.stan@gmail.com>
2014-05-27 14:08:41 +0300

    Fix string constructed from project parameter

--- src/main/g8/src/main/scala/$name__Camel$.scala
+++ src/main/g8/src/main/scala/$name__Camel$.scala
@@ -3,7 +3,7 @@ package $organization$.$name;format="camel"$
 import org.slf4j.LoggerFactory
 
 object $name;format="Camel"$ {
-  val logger = LoggerFactory.getLogger("$name;format="Camel"$Test")
+  val logger = LoggerFactory.getLogger("$name;format="Camel"$")
 
   def main(args: Array[String]): Unit = {
     logger.info("Running main...")
